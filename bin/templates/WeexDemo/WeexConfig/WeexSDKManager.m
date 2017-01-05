de8c4b94efe890ccc54137c8a6e53db7900e1b83
bofei.zq <bofei.zq@taobao.com>
2017-01-05 12:22:48 +0800

    add files

---
+++ bin/templates/WeexDemo/WeexConfig/WeexSDKManager.m
@@ -0,0 +1,68 @@
+//
+//  WeexSDKManager.m
+//  WeexDemo
+//
+//  Created by yangshengtao on 16/11/14.
+//  Copyright © 2016年 taobao. All rights reserved.
+//
+
+#import "WeexSDKManager.h"
+#import "DemoDefine.h"
+#import "WeexPlugin.h"
+#import <WeexSDK/WeexSDK.h>
+#import "WXDemoViewController.h"
+
+@implementation WeexSDKManager
+
++ (void)setupWithScanner:(BOOL)loadScanner;
+{
+    NSURL *url = nil;
+    WeexPlugin *loader = [WeexPlugin new];
+#if DEBUG
+    //If you are debugging in device , please change the host to current IP of your computer.
+    url = [loader jsBundleURL];
+    if (!url) {
+        url = [NSURL URLWithString:BUNDLE_URL];
+    }
+#else
+    url = [NSURL URLWithString:BUNDLE_URL];
+#endif
+    
+#ifdef UITEST
+    url = [NSURL URLWithString:UITEST_HOME_URL];
+#endif
+    
+    [self initWeexSDK];
+    
+    [loader registerWeexPlugin];
+    
+    if (loadScanner) {
+        [self loadCustomContainWithScannerWithUrl:url];
+    }else {
+        WXBaseViewController *demoController = [[WXBaseViewController alloc] initWithSourceURL:url];
+        [[UIApplication sharedApplication] delegate].window.rootViewController = [[WXRootViewController alloc] initWithRootViewController: demoController];
+    }
+}
+
++ (void)initWeexSDK
+{
+    [WXAppConfiguration setAppGroup:@"AliApp"];
+    [WXAppConfiguration setAppName:@"WeexDemo"];
+    [WXAppConfiguration setAppVersion:@"1.8.3"];
+    [WXAppConfiguration setExternalUserAgent:@"ExternalUA"];
+    
+    [WXSDKEngine initSDKEnviroment];
+    
+#ifdef DEBUG
+    [WXLog setLogLevel:WXLogLevelLog];
+#endif
+}
+
++ (void)loadCustomContainWithScannerWithUrl:(NSURL *)url
+{
+    UIViewController *demo = [[WXDemoViewController alloc] init];
+    ((WXDemoViewController *)demo).url = url;
+    [[UIApplication sharedApplication] delegate].window.rootViewController = [[WXRootViewController alloc] initWithRootViewController:demo];
+}
+
+@end
