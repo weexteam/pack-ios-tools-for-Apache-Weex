b2c737b672fca7a342f5123622c860dadc78f915
yangshengtao <yangshengtao1314@163.com>
2017-02-28 19:25:44 +0800

    update assert

--- bin/templates/WeexDemo/WeexConfig/WeexBundleUrlLoder.m
+++ bin/templates/WeexDemo/WeexConfig/WeexBundleUrlLoder.m
@@ -62,7 +62,7 @@
     
     // Assert file exists
     if (![[NSFileManager defaultManager] fileExistsAtPath:path]) {
-        NSAssert(NO, @"ERROR: %@ does not exist. Please run cordova-ios/bin/cordova_plist_to_config_xml path/to/project.", path);
+        NSAssert(NO, @"ERROR: %@ does not exist.", path);
         return nil;
     }
     
